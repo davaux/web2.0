f49487d9cb7e0e0da83b2d85c46d5ed5ded95168
davaux <davaux@protonmail.com>
2019-03-27 12:39:25 +0000

    Writing first query

---
+++ db.js
@@ -0,0 +1,31 @@
+var mysql = require('mysql');
+var db_config = {
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'supports',
+  database: 'webapp'
+}
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection(db_config);
+  connection.connect(function(err) {
+    if(err) {
+      console.log('error when connecting to db :', err)
+      setTimeout(handDisconnect, 2000);
+    }
+  });
+
+  connection.on('error', function(err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handDisconnect();
+    } else {
+      throw err
+    }
+  });
+}
+
+handleDisconnect();
+
+module.exports = connection;
